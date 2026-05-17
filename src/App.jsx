ca464d1380e27bd4650243c7ac2eb1806d57d1a1
Sayali99-FrontEnd <sayalidhole99@gmail.com>
2026-05-17 22:12:02 +0200

    next update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,59 @@
-
+import { useState} from "react";
+import styles from "./App.module.css";
+import {Chat} from "./components/Chat/Chat"
+import { Controls } from "./components/Controls/Contols";
 
 function App() {
+  
+  const [ messages, setMessages] = useState(MESSAGES)
 
   return (
-    <>
-      <h1>React AI Chatbot</h1>
-    </>
+    <div className={styles.App}>
+     <header className={styles.Header}>
+     <img className = {styles.Logo} src ="/chat-bot.png"/>
+     <h2 className={styles.Title}>React AI Chatbot</h2>
+     </header>
+     <div className={styles.ChatContainer}>
+        <Chat messages = {messages} />
+     </div>
+        <Controls />
+    </div>
   )
 }
+
+const MESSAGES = [
+  {
+    role: "user",
+    content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde dicta ducimus, incidunt, asperiores modi fugit facere nemo similique atque quo culpa? Nihil tenetur quod quas odio veritatis quasi, in vel?"
+  },
+  {
+    role: "assistant",
+    content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde dicta ducimus, incidunt, asperiores modi fugit facere nemo similique atque quo culpa? Nihil tenetur quod quas odio veritatis quasi, in vel?"
+  },
+  {
+    role: "user",
+    content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde dicta ducimus, incidunt, asperiores modi fugit facere nemo similique atque quo culpa? Nihil tenetur quod quas odio veritatis quasi, in vel?"
+  },
+  {
+    role: "assistant",
+    content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde dicta ducimus, incidunt, asperiores modi fugit facere nemo similique atque quo culpa? Nihil tenetur quod quas odio veritatis quasi, in vel?"
+  },
+  {
+    role: "user",
+    content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde dicta ducimus, incidunt, asperiores modi fugit facere nemo similique atque quo culpa? Nihil tenetur quod quas odio veritatis quasi, in vel?"
+  },
+  {
+    role: "assistant",
+    content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde dicta ducimus, incidunt, asperiores modi fugit facere nemo similique atque quo culpa? Nihil tenetur quod quas odio veritatis quasi, in vel?"
+  },  {
+    role: "user",
+    content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde dicta ducimus, incidunt, asperiores modi fugit facere nemo similique atque quo culpa? Nihil tenetur quod quas odio veritatis quasi, in vel?"
+  },
+  {
+    role: "assistant",
+    content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde dicta ducimus, incidunt, asperiores modi fugit facere nemo similique atque quo culpa? Nihil tenetur quod quas odio veritatis quasi, in vel?"
+  }
+]
+
 
 export default App
---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,14 @@
+import styles from "./Chat.module.css";
+
+export function Chat({ messages }) {
+    return (
+        <div className={styles.Chat}>
+            {messages.map(({ role, content }, index) => {
+                <div className= {styles.messages} key={index} data-role={role}>
+                    {content}
+                </div>
+            })}
+
+        </div>
+    )
+}
